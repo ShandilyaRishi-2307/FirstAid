3c7cbc49586f7ef769f4a6592a8a76b5b393a043
Rishi Pandey <shandilyarishi.2005@gmail.com>
2026-06-18 23:25:23 +0530

    added emergencyPage

---
+++ emergencyPage/emergency.js
@@ -0,0 +1,46 @@
+const demo = document.getElementById("demo");
+
+function getLocation(){
+
+    if(navigator.geolocation){
+
+        navigator.geolocation.getCurrentPosition(showPosition);
+
+    }
+    else{
+        demo.innerHTML = "Geolocation is not supported.";
+    }
+}
+
+function showPosition(position){
+
+    demo.innerHTML =
+    "Latitude : " +
+    position.coords.latitude +
+    "<br>Longitude : " +
+    position.coords.longitude;
+}
+
+
+// Nearest Hospital button
+
+const hospitalBtn = document.getElementById("hospitalBtn");
+
+hospitalBtn.addEventListener("click",()=>{
+
+    if(navigator.geolocation){
+
+        navigator.geolocation.getCurrentPosition((position)=>{
+
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+
+            window.open(
+`https://www.google.com/maps/search/hospitals/@${lat},${lon},15z`
+            );
+
+        });
+
+    }
+
+});
