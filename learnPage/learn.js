306feb5aa590c89b9780d097a72014644df5a3bc
Rishi Pandey <shandilyarishi.2005@gmail.com>
2026-06-24 02:38:42 +0530

    added search bar

--- learnPage/learn.js
+++ learnPage/learn.js
@@ -1,3 +1,34 @@
 function openPage(card) {
     window.location.href = `/learnPage/explain/${card.id}.html`;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+        const searchInput = document.getElementById("searchInput");
+        const cards = document.querySelectorAll(".topics .card");
+        const noResultsMsg = document.getElementById("no-results");
+
+        searchInput.addEventListener("input", (e) => {
+           
+            const query = e.target.value.toLowerCase().trim();
+            let hasVisibleCards = false;
+
+            cards.forEach(card => {
+             
+                const topicName = card.textContent.toLowerCase();
+
+            
+                if (topicName.includes(query)) {
+                    card.style.display = ""; // Shows the card
+                    hasVisibleCards = true;
+                } else {
+                    card.style.display = "none"; 
+                }
+            });
+
+            if (hasVisibleCards) {
+                noResultsMsg.style.display = "none";
+            } else {
+                noResultsMsg.style.display = "block";
+            }
+        });
+    });
